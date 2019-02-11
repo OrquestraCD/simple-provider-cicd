69556bc9292393c73d7ad589f98d97e7df92c74d
Matthew Bonig <matthew.bonig@gmail.com>
2019-02-11 13:16:57 -0700

    removing public access

--- modules/codepipeline/s3.tf
+++ modules/codepipeline/s3.tf
@@ -6,20 +6,4 @@ resource "aws_s3_bucket" "build-bucket" {
 resource "aws_s3_bucket" "deploy-bucket" {
   bucket = "${var.deploy_s3_bucket}"
   acl    = "private"
-
-  policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Id": "Policy1548346025552",
-    "Statement": [
-        {
-            "Sid": "PublicRead",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::${var.deploy_s3_bucket}/*"
-        }
-    ]
-}
-EOF
 }
